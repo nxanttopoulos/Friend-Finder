97e3ae7ed9989724bfcf9bc6763842452e7e6058
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-05-11 11:39:57 -0400

    fixes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,6 @@
 // DATA
 // Below data will hold all of potential friends.
 var friendArray = [
-  {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-    "5",
-    "1",
-    "4",
-    "4",
-    "5",
-    "1",
-    "2",
-    "5",
-    "4",
-    "1"
-    ]
-  },
 {
 	name: "Jacob Deming",
 	photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
@@ -79,22 +63,6 @@ var friendArray = [
 	"3",
 	"1",
 	"4"
-	]
-  },
-{
-	name: "Jordan Biason",
-	photo: "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
-	scores: [
-	"4",
-	"4",
-	"2",
-	"3",
-	"2",
-	"2",
-	"3",
-	"2",
-	"4",
-	"5"
 	]
   },
 {
